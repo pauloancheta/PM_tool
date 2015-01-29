e97ffb08c413dec38697684d11737bbdc755cd19
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-01-28 17:45:23 -0800

    new user redirected to new project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      projects = Project.where(user_id: current_user.id ).count
+      if projects == 0
+        redirect_to new_project_path
+      end
+    end
   end
 
   def about
